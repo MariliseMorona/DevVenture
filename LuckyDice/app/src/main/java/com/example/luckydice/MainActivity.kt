eeb39a5c7bcae91750f86d6fbfb30491f0e9eebb
Marilise Morona <marymorona@gmail.com>
2021-06-09 20:57:29 -0300

    Add Color Button and Images Dice

--- LuckyDice/app/src/main/java/com/example/luckydice/MainActivity.kt
+++ LuckyDice/app/src/main/java/com/example/luckydice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.luckydice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,18 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dado01 = findViewById<TextView>(R.id.txt_dado01)
-        val dado02 = findViewById<TextView>(R.id.txt_dado02)
+        val dado01 = findViewById<ImageView>(R.id.mgv_dado01)
+        val dado02 = findViewById<ImageView>(R.id.mgv_dado02)
         val btn = findViewById<Button>(R.id.btn_jogar)
 
+        val images = listOf(
+            R.drawable.dice_1, R.drawable.dice_2, R.drawable.dice_3,
+            R.drawable.dice_4, R.drawable.dice_5, R.drawable.dice_6)
+
         btn.setOnClickListener{
-            dado01.text = geraAleatorio().toString()
-            dado02.text = geraAleatorio().toString()
+            dado01.setImageResource(images.get(geraAleatorio()))
+            dado02.setImageResource(images.get(geraAleatorio()))
+
+            //OR
+            //dado01.setImageResource(images[geraAleatorio()-1])
+            //dado02.setImageResource(images[geraAleatorio()-1])
+
+            //Ou, não precisando usar a função geraAleatorio
+            //dado01.setImageResource(images.random())
+            //dado02.setImageResource(images.random())
         }
 
     }
 
     private fun geraAleatorio(): Int{
-        return (1..6).random()
+        return (0..5).random()
     }
 }
