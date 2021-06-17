4fb76566d62d94b8087fd1d51b3de8900a124497
Marilise Morona <marymorona@gmail.com>
2021-06-16 22:00:56 -0300

    Insert buttons

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -7,5 +7,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_one.setOnClickListener
+
     }
+
+    override fun insertValue(string: String, clearData: Boolean){
+        if(txt_results.text.isNotEmpty()){
+            expressao.text = ""
+        }
+        if(clearData){
+            txt_results = ""
+            expressao.append(string)
+        }else{
+            expressao.append(txt_results.text)
+            expressao.append(string)
+            txt_results.text = ""
+        }
+    }
+
 }
